9a7e2751bdc806b4c034e9e33a4554780f5e5fc4
Tony <antonynganga247@gmail.com>
2021-02-23 17:34:16 +0300

    second

---
+++ app/src/main/java/com/example/medicalconsultation/FirebaseUtils.java
@@ -0,0 +1,43 @@
+package com.example.medicalconsultation;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
+public class FirebaseUtils {
+    private static final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // Create a new user with a first and last name
+    public static void registerUser(UserDetails user){
+        db.collection("patientUsers")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+
+
+// Add a new document with a generated ID
+
+
+}
