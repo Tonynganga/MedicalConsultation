fd08464209e4f567cb6a0fb9842d374ca27cae4a
vicmwass <vicmwas2017@gmail.com>
2021-02-25 13:31:17 +0300

    Added data retrieval of patient problem for patient and doctor,connected PatientHomePage with PatientProblem activity,DoctorHomePage with PostComments activity,PatientHomePage with CommentSection activity

--- app/src/main/java/com/example/medicalconsultation/FirebaseUtils.java
+++ app/src/main/java/com/example/medicalconsultation/FirebaseUtils.java
@@ -4,21 +4,28 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.example.medicalconsultation.HelperClasses.Comment;
 import com.example.medicalconsultation.HelperClasses.Doctor;
 import com.example.medicalconsultation.HelperClasses.Patient;
+import com.example.medicalconsultation.HelperClasses.PatientPost;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 import static android.content.ContentValues.TAG;
 
 public class FirebaseUtils {
-    private static final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public static final FirebaseFirestore mFireStore = FirebaseFirestore.getInstance();
+    public static final FirebaseAuth mFirebaseAuth= FirebaseAuth.getInstance();
+
 
     // Create a new user with a first and last name
     public static void registerPatientUser(Patient user){
-        db.collection("patientUsers")
+        mFireStore.collection("patientUsers")
                 .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
@@ -35,7 +42,7 @@ public class FirebaseUtils {
 
     }
     public static void registerDoctorUser(Doctor user){
-        db.collection("doctorUsers")
+        mFireStore.collection("doctorUsers")
                 .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
@@ -51,6 +58,48 @@ public class FirebaseUtils {
                 });
 
     }
+    public static void addPost(PatientPost post){
+        final DocumentReference PatientPostRef;
+        PatientPostRef = mFireStore.collection("problems")
+                .document();
+        post.setId( PatientPostRef.getId());
+        PatientPostRef.set(post)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Firestore", "Document updated with ID: " + PatientPostRef.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Firestore", "Error updating document", e);
+                    }
+                });
+    }
+    public static void addComment(Comment comment,String probId){
+        final DocumentReference commentRef;
+        commentRef = mFireStore.collection("problems")
+                .document(probId)
+                .collection("comments")
+                .document();
+        comment.setId( commentRef.getId());
+        commentRef.set(comment)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Firestore", "Document updated with ID: " + commentRef.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Firestore", "Error updating document", e);
+                    }
+                });
+
+    }
+
 
 
 // Add a new document with a generated ID
--- app/src/main/java/com/example/medicalconsultation/PostComments.java
+++ app/src/main/java/com/example/medicalconsultation/PostComments.java
@@ -2,7 +2,11 @@ package com.example.medicalconsultation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.example.medicalconsultation.HelperClasses.PatientPostAdapter.PROBLEM_DESCRIPTION;
 
 public class PostComments extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class PostComments extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_comments);
+        Intent myIntent =getIntent();
+        String description=myIntent.getStringExtra(PROBLEM_DESCRIPTION);
+        TextView probDesc=findViewById(R.id.tvDescForDoc);
+        probDesc.setText(description);
     }
 }
